a50ea776778c10c298b4706f675f19b1938ea313
Minji So <sskkanji@gmail.com>
2024-01-24 14:53:52 +0900

    Modify layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Multiple theme sample",
-  description: "3가지 이상 테마 적용한 프로젝트 예시",
+  title: 'Multiple theme sample',
+  description: '3가지 이상 테마 적용한 프로젝트 예시',
 };
 
 export default function RootLayout({
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="gray">
       <body className={inter.className}>{children}</body>
     </html>
   );
